f2bb7eed806bda98b4d1eaa5d876fb371531387c
Taichi <takatobi002@gmail.com>
2023-09-15 20:30:41 +0900

    add follow func

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,14 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TweetController;
 use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\FollowController;
 
-// コメントは省略
+
 
 // 🔽 ここを編集
 Route::middleware('auth')->group(function () {
+  Route::post('user/{user}/follow', [FollowController::class, 'store'])->name('follow');
+  Route::post('user/{user}/unfollow', [FollowController::class, 'destroy'])->name('unfollow');
   Route::post('tweet/{tweet}/favorites', [FavoriteController::class, 'store'])->name('favorites');
   Route::post('tweet/{tweet}/unfavorites', [FavoriteController::class, 'destroy'])->name('unfavorites');
   Route::get('/tweet/mypage', [TweetController::class, 'mydata'])->name('tweet.mypage');
